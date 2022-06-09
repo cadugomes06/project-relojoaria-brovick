61ba92045da62cdc1b94f44e645849ad822acea8
Carlos Eduardo <cadu.010808@gmail.com>
2022-06-08 22:20:48 -0300

    Carrinho adicionando valor ao click e itens armazenados no local storage

--- script.js
+++ script.js
@@ -21,42 +21,101 @@ function ativandoSlide() {
 ativandoSlide();
 /* Slide de produtos exclusivos */
 
-
-
 /* Carrinho de compras */
-const carrinho = document.querySelector(".carrinho");
+const car = document.querySelector(".carrinho");
 const menuCarrinho = document.querySelector(".sessao-carrinho");
-const produtos = document.querySelectorAll('.addItens'); 
-const listaItens = document.querySelectorAll('.lista-itens')
+const btnAdicionar = document.querySelectorAll('.addItens'); 
+const rel = document.querySelectorAll('.relogio')
+
+let relogios = [
+  {
+    name: 'Brovick-pro',
+    tag: 'relogiopro',
+    price: 700,
+    inCart: 0,
+  },
+  {
+    name: 'Brovick-low',
+    tag: 'relogiolow',
+    price: 600,
+    inCart: 0,
+  },
+  {
+    name: 'Brovick-lit',
+    tag: 'relogiolit',
+    price: 650,
+    inCart: 0,
+  },
+  {
+    name: 'Smart-vik',
+    tag: 'smartvik',
+    price: 500,
+    inCart: 0,
+  },
+  {
+    name: 'Sport-vik',
+    tag: 'sportvik',
+    price: 350,
+    inCart: 0,
+  },
+  {
+    name: 'Edge-vik',
+    tag: 'edgevik',
+    price: 350,
+    inCart: 0,
+  },
+]
 
 function toggleMenu() {
-  menuCarrinho.classList.toggle("active");
-  
+    menuCarrinho.classList.toggle("active");  
+}
+car.addEventListener("click", toggleMenu);
+
+for (let i = 0; i < btnAdicionar.length; i++ ) {
+  btnAdicionar[i].addEventListener('click', () => {
+    cartNumbers(relogios[i]);
+  })
 }
 
-function handleProduto (produto, index, lista) {
-    produto =  ['brovick-pro','brovick-low','brovick-lit','smart-vik','sport-vik','edge-vik']
-
-  if (menuCarrinho.className.endsWith('active')){
-
-  if (index === 0) {
-        menuCarrinho.innerHTML = `<li>${produto[0]} Adicionado</>`  
-        console.log(menuCarrinho)      
-  }
-  if (index === 1) {
-    menuCarrinho.innerHTML = `<li>${produto[1]} Adicionado</li>`
-    console.log(menuCarrinho)    
-  }
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem('cartNumbers')   // manter o valor do storage no refresh
+  if ( productNumbers ) {
+    document.querySelector('.valueCart').textContent = productNumbers
   }
 }
 
-carrinho.addEventListener("click", toggleMenu);
 
-produtos.forEach((produto, index) => {
-    produto.addEventListener('click', () => {
-        handleProduto(produto, index)
-    })
-})
+function cartNumbers(relogios) {
+
+  productNumbers = localStorage.getItem('cartNumbers')
+  productNumbers = parseInt(productNumbers)
+
+  if ( productNumbers ) {
+    localStorage.setItem('cartNumbers', productNumbers + 1)
+    document.querySelector('.valueCart').textContent = productNumbers + 1
+  } else {
+    localStorage.setItem('cartNumbers', 1)
+    document.querySelector('.valueCart').textContent = 1
+  }
+ setItens(relogios)
+}
+
+function setItens(relogios) {
+  let cartItens = localStorage.getItem('relogiosInCart')
+
+  if (relogios.inCart == 0) {
+    relogios.inCart = 1
+  }  else {
+    relogios.inCart += 1
+  }
 
 
-//menuCarrinho.className.endsWith('active')
+  console.log(cartItens)
+  cartItens = {
+    [relogios.tag]: relogios
+  }
+  localStorage.setItem('relogiosInCart', JSON.stringify(cartItens))
+
+}
+
+onLoadCartNumbers()
